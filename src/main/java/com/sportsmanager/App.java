20b07bb6a91d6a11ef56b541a14afa43228b9d3b
yigitkoparan <yigitkoparan@hotmail.com>
2026-03-28 16:25:16 +0300

    Initial commit

---
+++ src/main/java/com/sportsmanager/App.java
@@ -0,0 +1,13 @@
+package com.sportsmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
