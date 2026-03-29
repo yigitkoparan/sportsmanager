b7d5175fd9ddda096d7267f8c8033b912aab9a4d
yigitkoparan <yigitkoparan@hotmail.com>
2026-03-29 22:35:30 +0300

    Create Match.java

---
+++ src/main/java/com/sportsmanager/framework/Match.java
@@ -0,0 +1,14 @@
+package com.sportsmanager.framework;
+
+public abstract class Match {
+    String homeTeam;
+    String awayTeam;
+    int homeScore;
+    int awayScore;
+
+        public Match(String homeTeam, String awayTeam){
+            this.homeTeam = homeTeam;
+            this.awayTeam = awayTeam;
+        }
+
+}
