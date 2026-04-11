291ca2b34bc36020d85aedb9949c3d7559ac422a
20220602049 <cagankaratas23@gmail.com>
2026-04-11 15:15:39 +0300

    Update Match.java

--- src/main/java/com/sportsmanager/framework/Match.java
+++ src/main/java/com/sportsmanager/framework/Match.java
@@ -1,14 +1,31 @@
 package com.sportsmanager.framework;
 
 public abstract class Match {
-    protected int homeScore;
     protected int awayScore;
+    protected int homeScore;
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public void setHomeScore(int homeScore) {
+        this.homeScore = homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    public void setAwayScore(int awayScore) {
+        this.awayScore = awayScore;
+    }
+
 
 
         public Match(int homeScore, int awayScore){
             this.homeScore = 0;
             this.awayScore = 0;
         }
+
 
         public abstract void simulate();
 }
